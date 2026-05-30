a40d4326136f9bb68373046819a7bf42e8c823c9
ＳＰＡＲＫＹ'ＮＯＸ <senpaineji7@gmail.com>
2026-05-30 14:45:31 +0530

    Update LumiXApp.kt

--- app/src/main/java/com/sparkynox/lumix/LumiXApp.kt
+++ app/src/main/java/com/sparkynox/lumix/LumiXApp.kt
@@ -1,5 +1,11 @@
 package com.sparkynox.lumix
 
 import androidx.multidex.MultiDexApplication
+import com.sparkynox.lumix.helper.YtDlpInstaller
 
-class LumiXApp : MultiDexApplication()
+class LumiXApp : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        Thread { YtDlpInstaller.install(this) }.start()
+    }
+}
