a3d99e89b2ae970992add97b1a42b13bbd0734d2
ＳＰＡＲＫＹ'ＮＯＸ <senpaineji7@gmail.com>
2026-05-31 15:07:57 +0530

    Add files via upload

--- app/src/main/java/com/sparkynox/lumix/LumiXApp.kt
+++ app/src/main/java/com/sparkynox/lumix/LumiXApp.kt
@@ -1,9 +1,11 @@
 package com.sparkynox.lumix
 
 import androidx.multidex.MultiDexApplication
+import com.sparkynox.lumix.helper.YtDlpHelper
 
 class LumiXApp : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
+        Thread { YtDlpHelper.init() }.start()
     }
 }
